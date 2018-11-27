26b5b1ac632c77d9c1637a3e3c073c8bf52ee2c6
Jonathan Cardoso Machado <me@jonathancardoso.com>
2018-11-27 09:06:05 -0200

    add suspense to ContributorItem file

--- src/components/contributor/ContributorItem.js
+++ src/components/contributor/ContributorItem.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+
+import { createResourceFetcher } from '../../cache';
+import { LoadingSpinnerImage } from '../LoadingSpinner';
+
+const imageFetcher = createResourceFetcher(src => {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.onload = () => resolve(src);
+    image.src = src;
+  });
+});
+
+function ContributorAvatar(props) {
+  return <img {...props} src={imageFetcher.read(props.src)} />;
+}
 
 export function ContributorItem(props) {
   const { contributor } = props;
   return (
     <li key={contributor.node_id}>
-      <img className="contributor-image" alt="" src={contributor.avatar_url} />
+      <Suspense fallback={<LoadingSpinnerImage className="small" />}>
+        <ContributorAvatar
+          className="contributor-image"
+          alt=""
+          src={contributor.avatar_url}
+        />
+      </Suspense>
       <span className="contributor-login">{contributor.login}</span>
     </li>
   );

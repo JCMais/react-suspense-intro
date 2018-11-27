54ff48c6995bcdb60e87698f10967c2173a7506c
Jonathan Cardoso Machado <me@jonathancardoso.com>
2018-11-27 08:30:54 -0200

    add contributorsFetcher

--- src/api/fetchers.js
+++ src/api/fetchers.js
@@ -1,7 +1,11 @@
 import { createResourceFetcher } from '../cache';
 
-import { getOrgRepos } from './api';
+import { getOrgRepos, getRepoContributors } from './api';
 
 export const reposFetcher = createResourceFetcher(orgName =>
   getOrgRepos(orgName)
+);
+
+export const contributorsFetcher = createResourceFetcher(fullRepoName =>
+  getRepoContributors(fullRepoName)
 );
